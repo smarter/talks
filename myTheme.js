749a999a988298b2ce61b8966d7b8a900ba4f993
Guillaume Martres <smarter@ubuntu.com>
2019-06-08 02:39:28 +0200

    add missing file

---
+++ myTheme.js
@@ -0,0 +1,5 @@
+export default {
+  font: "'Source Sans Pro', sans-serif",
+  monospace: "'Fira Mono', monospace",
+  googleFont: "https://fonts.googleapis.com/css?family=Fira+Mono|Source+Sans+Pro&display=swap"
+}
