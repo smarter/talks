6cc9472ffd32e4e63a99701a04c2dc2a579e16b4
Guillaume Martres <smarter@ubuntu.com>
2019-08-27 16:57:05 +0200

    Fix emoji support for chrome

--- myTheme.js
+++ myTheme.js
@@ -1,3 +1,20 @@
+const white = '#fffceb'
+const black = '#11111f'
+const blue = '#2d5dd7'
+
+// Based on book theme
+export default {
+  fonts: {
+    body: '"Crimson Text", Noto Color Emoji, serif',
+  },
+  googleFont: 'https://fonts.googleapis.com/css?family=Fira+Mono|Source+Sans+Pro|Crimson+Text',
+  colors: {
+    text: black,
+    background: white,
+    link: blue,
+  },
+}
+
 // const Provider = props => (
 //     <div css={{
 //       width: "100%",
@@ -8,14 +25,15 @@
 // )
 
 
-export default {
-  // Provider,
-  Slide: {
-    '& > div': {
-      width: "80%",
-    }
-  },
-  font: "'Source Sans Pro', sans-serif",
-  monospace: "'Fira Mono', monospace",
-  googleFont: "https://fonts.googleapis.com/css?family=Fira+Mono|Source+Sans+Pro&display=swap"
-}
+// export default { 
+//  // Provider,
+//   // Slide: {
+//   //   '& > div': {
+//   //     width: "80%",
+//   //   }
+//   // },
+//   // font: "'Source Sans Pro', sans-serif",
+//   // monospace: "'Fira Mono', monospace",
+//   // googleFont: "https://fonts.googleapis.com/css?family=Fira+Mono|Source+Sans+Pro&display=swap"
+
+// }
